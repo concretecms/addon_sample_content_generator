0adf17236b0ce7de22112ce099fbf5bc1abf88c3
Ryan Hewitt <ryan@mesuva.com.au>
2015-06-01 18:03:18 +0930

    Extra option to filter output XML by specified nodes

--- packages/sample_content_generator/controllers/single_page/dashboard/generate_install_data.php
+++ packages/sample_content_generator/controllers/single_page/dashboard/generate_install_data.php
@@ -4,7 +4,19 @@ namespace Concrete\Package\SampleContentGenerator\Controller\SinglePage\Dashboar
 use \Concrete\Core\Page\Controller\PageController;
 use \Concrete\Core\Backup\ContentExporter;
 use Loader;
+use View;
+
 class GenerateInstallData extends PageController {
+    public function view() {
+        $v = View::getInstance();
+        $v->requireAsset('select2');
+
+        $defaultNodes = array('pagetypes', 'attributekeys', 'attributesets', 'pagetemplates', 'themes', 'stacks', 'pages', 'sociallinks','pagefeeds');
+        $allNodes = array('attributecategories','features','featurecategories','conversationeditors','conversationratingtypes','pagetypepublishtargettypes','composercontroltypes','pagetypes','attributetypes','blocktypes','blocktypesets','gatheringsources','gatheringitemtemplates','attributekeys', 'attributesets', 'pagetemplates', 'themes','packages','permissionaccessentitytypes','permissionkeys','workflowtypes','jobs','singlepages','stacks', 'pages', 'systemcaptcha','sociallinks','pagefeeds','thumbnailtypes','trees');
+
+        $this->set('defaultNodes', $defaultNodes);
+        $this->set('allNodes', $allNodes);
+    }
 
 	public function get_files() {
 		$export = new ContentExporter();
@@ -19,13 +31,45 @@ class GenerateInstallData extends PageController {
 		$export->removeItem('singlepages', 'page', 'sample_content_generator');
 		// check packages
 		$xml = $export->output();
-		
+
 		$th = Loader::helper('text');
 		$xml = $th->formatXML($xml);
-		
+
 		$this->set('outputContent', $xml);
 	}
-	
-	
-	
+
+    public function get_custom_content_xml() {
+        $export = new ContentExporter();
+        $export->run();
+        $export->removeItem('packages', 'package', 'sample_content_generator');
+        $export->removeItem('singlepages', 'page', 'sample_content_generator');
+        // check packages
+        $xml = $export->output();
+
+        $th = Loader::helper('text');
+        $xml = $th->formatXML($xml);
+
+        $nodestooutput = $this->post('nodes');
+
+        $dom = new \DOMDocument('1.0');
+        $dom->preserveWhiteSpace = false;
+        $dom->formatOutput = true;
+        $dom->loadXML($xml);
+
+        $domElemsToRemove = array();
+
+        foreach($dom->childNodes as $child) {
+            foreach($child->childNodes as $c) {
+                if (!in_array($c->nodeName, $nodestooutput)) {
+                    $domElemsToRemove[] = $c;
+                }
+            }
+        }
+
+        foreach( $domElemsToRemove as $domElement ){
+            $domElement->parentNode->removeChild($domElement);
+        }
+
+        $this->set('outputContent', $dom->saveXML());
+    }
 }
--- packages/sample_content_generator/single_pages/dashboard/generate_install_data.php
+++ packages/sample_content_generator/single_pages/dashboard/generate_install_data.php
@@ -11,7 +11,7 @@
 	<a class="btn" href="<?=$this->url('/dashboard/generate_install_data')?>"><?=t('Back')?></a>
 
 
-<? } else if ($this->controller->getTask() == 'get_content_xml') { ?>
+<? } else if ($this->controller->getTask() == 'get_content_xml' | $this->controller->getTask() == 'get_custom_content_xml') { ?>
 
 <h3><?=t('Data')?></h3>
 
@@ -31,11 +31,35 @@
 
 <h3><?=t('Generate content.xml from current website.')?></h3>
 <p><?=t('Once you generate this file you can include it in a custom starting point.')?></p>
+<br />
+<h4><?php echo t('Full Export'); ?></h4>
 <form method="post" action="<?=$this->action('get_content_xml')?>">
-    <button type="submit" class="btn btn-default"><?=t('Go')?></button>
+    <p><?php echo t('Generate XML for all site data');?></p>
+    <button type="submit" class="btn btn-default"><?=t('Generate Complete Site XML')?></button>
 </form>
+<br />
+    <h4><?php echo t('Custom Export');?></h4>
+    <form method="post" action="<?=$this->action('get_custom_content_xml')?>">
+        <p><?php echo t('Generate XML with the following nodes/data:');?></p>
+        <div class="ccm-search-field-content ccm-search-field-content-select2">
+            <select multiple="multiple" name="nodes[]" class="nodelist" >
+                <?php foreach ($allNodes as $node) { ?>
+                    <option value="<?php echo $node;?>" <?php echo (in_array($node, $defaultNodes) ? 'selected="selected"' : ''); ?>>  <?php echo $node; ?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <span class="help-block"><?php echo t('Data most commonly used in starting points has been pre-selected'); ?></span>
+        <button type="submit" class="btn btn-default"><?=t('Generate Custom Site XML')?></button>
+</form>
+
 <div class="ccm-spacer"></div>
 
 <? } ?>
 
 <?=Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper();?>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.nodelist').select2();
+    });
+</script>
